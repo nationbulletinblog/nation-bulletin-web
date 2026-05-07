636a6d5989b75eaa194310234a9083033c580dca
pratik-senjaliya <pratiksenjaliya7@gmail.com>
2026-05-07 13:15:44 +0530

    sitemap issue fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { client } from '@/lib/sanity.client';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
